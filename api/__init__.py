7b9003bb4dbd25e6b1aaffe93ce4acf83daa8bab
chetanchadalavada <@Romans614>
2025-10-09 20:47:55 -0600

    cleanup

--- api/__init__.py
+++ api/__init__.py
@@ -1,16 +1,16 @@
-import os
-import django
-import sys
-from django.conf import settings
+# import os
+# import django
+# import sys
+# from django.conf import settings
 
-# Configure Django settings
-sys.path.append(os.path.dirname(__file__))
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+# # Configure Django settings
+# sys.path.append(os.path.dirname(__file__))
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 
-# Configure Django
-django.setup()
+# # Configure Django
+# django.setup()
 
-# Expose the WSGI application
-from api.wsgi import application
+# # Expose the WSGI application
+# from api.wsgi import application
 
-app = application
+# app = application
