dd8970b9991b31d5c0d5d3cf1ae6d2c35517a327
Chetan Chadalavada <chetan.chadalavada@gmail.com>
2025-10-09 19:14:55 -0600

    Fix build issue

--- api/__init__.py
+++ api/__init__.py
@@ -1,13 +1,16 @@
 import os
 import django
+import sys
 from django.conf import settings
 
 # Configure Django settings
+sys.path.append(os.path.dirname(__file__))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+
+# Configure Django
 django.setup()
 
-# Expose the app instance for Vercel
+# Expose the WSGI application
 from api.wsgi import application
 
-def handler(request, *args, **kwargs):
-    return application(request, *args, **kwargs)
+app = application
